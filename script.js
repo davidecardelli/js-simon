3188c81c59f36118a16b3b87eb2398826967dc45
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-20 16:36:46 +0100

    item recovery + fixed date

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log('OK');
 
+// Recupero elementi dalla pagina
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+// Recupero la data di Natale per intero
+
+const christmas = new Date ("Dec 25 2022 00:00:00");
+const christmasTime = christmas.getTime();
+console.log(christmasTime);
